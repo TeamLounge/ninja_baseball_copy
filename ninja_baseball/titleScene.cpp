de81f3506b3877c6d662bada322e456910057394
LAPTOP-09EAPCUQ\yeari <yearinan@gmail.com>
2021-06-25 15:43:58 +0900

    타이틀로고 이미지 추가

--- ninja_baseball/titleScene.cpp
+++ ninja_baseball/titleScene.cpp
@@ -5,6 +5,8 @@ HRESULT titleScene::init()
 {
 	IMAGEMANAGER->addImage("title_background", "image/6_UI/title/title_background.bmp", WINSIZEX, WINSIZEY, false, RGB(255,0,255), false);
 	IMAGEMANAGER->addImage("silhouette", "image/6_UI/title/silhouette.bmp", 900, 510, true, RGB(255, 0, 255), false);
+	IMAGEMANAGER->addFrameImage("title_frame", "image/6_UI/title/title_frame.bmp", 11745, 420, 15, 1, true, RGB(255, 0, 255), false);
+	
 	_silhouette = RectMakeCenter(WINSIZEX / 2, WINSIZEY + IMAGEMANAGER->findImage("silhouette")->getHeight()/2, IMAGEMANAGER->findImage("silhouette")->getWidth(), IMAGEMANAGER->findImage("silhouette")->getHeight());
 	CAMERAMANAGER->setCamera(0, 0);
 	return S_OK;
@@ -32,4 +34,6 @@ void titleScene::render()
 {
 	IMAGEMANAGER->findImage("title_background")->render(getMemDC(), 0, 0);
 	IMAGEMANAGER->findImage("silhouette")->render(getMemDC(), _silhouette.left, _silhouette.top);
+
+	IMAGEMANAGER->findImage("title_frame")->frameRender(getMemDC(), 150, 30, 0, 0);
 }
