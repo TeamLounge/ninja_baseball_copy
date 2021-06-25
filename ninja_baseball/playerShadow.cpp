d5184c6902f8f533150c48abff9fe3f0accc3ceb
박동철 <ehdcjf1123@naver.com>
2021-06-25 09:27:18 +0900

    그림자 겟터,셋터 추가

---
+++ ninja_baseball/playerShadow.cpp
@@ -0,0 +1,19 @@
+#include "stdafx.h"
+#include "playerShadow.h"
+
+HRESULT playerShadow::init()
+{
+	return S_OK;
+}
+
+void playerShadow::release()
+{
+}
+
+void playerShadow::update()
+{
+}
+
+void playerShadow::render()
+{
+}
---
+++ ninja_baseball/playerShadow.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "gameNode.h"
+
+class playerShadow :public gameNode
+{
+private:
+	
+	float _x;
+	float _y;
+	RECT _rc;
+	image* _shadowImage;
+
+public:
+
+	virtual HRESULT init();
+	virtual void release();
+	virtual void update();
+	virtual void render();
+
+	//그림자 겟터
+	float getShadowX() { return _x; }
+	float getShadowY() { return _y; }
+	RECT getShadowRect() { return _rc; }
+	image* getShadowImage() { return _shadowImage; }
+
+	//그림자 셋터
+	void setShadowX(float x) { _x = x; }
+	void setShadowY(float y) { _y = y; }
+	void setShadowRect(RECT rc) { _rc = rc; }
+	void setShadowImage(image* shadowImage) { _shadowImage = shadowImage; }
+};
+
