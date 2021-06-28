6629afb9de9785b7c088aaea1d501891380c24b8
박동철 <ehdcjf1123@naver.com>
2021-06-28 09:13:24 +0900

    red_runState추가

--- ninja_baseball/red_runState.h
+++ ninja_baseball/red_runState.h
@@ -1,19 +1,16 @@
 #pragma once
 #include "playerstate.h"
 
-#define redSpeed 10
-
-class red_moveState : public playerstate
+class red_runState : public playerstate
 {
 private:
+
 	RECT _rc;
 	int _count;
 	int _index;
-	int _runTime;
-	bool _runTimeStart;
-	bool _isRun;
 
 public:
+
 	playerstate* handleInput(player* _player);
 	void update(player* _player);
 	void enter(player* _player);
