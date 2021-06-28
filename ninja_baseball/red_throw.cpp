d2a2f1a39bda733cd56d486e4b87a7a29b332ac0
박동철 <ehdcjf1123@naver.com>
2021-06-28 13:24:12 +0900

    red_throwState 수정

--- ninja_baseball/red_throw.cpp
+++ ninja_baseball/red_throw.cpp
@@ -4,14 +4,62 @@
 
 playerstate * red_throw::handleInput(player * _player)
 {
+	if (_time > 30)
+	{
+		return new red_idleState;
+	}
+
 	return nullptr;
 }
 
 void red_throw::update(player * _player)
 {
+	_count++;
+
+	if (_count % 5 == 0)
+	{
+		if (_player->isRight == true)
+		{
+			_player->getImage()->setFrameX(_index);
+			_player->getImage()->setFrameY(0);
+			_index++;
+		}
+
+		if (_player->isRight == false)
+		{
+			_player->getImage()->setFrameX(_index);
+			_player->getImage()->setFrameY(1);
+			_index++;
+		}
+
+		_count = 0;
+	}
+
+	if (_player->getImage()->getMaxFrameX() < _index)
+	{
+		_time++;
+	}
 }
 
 void red_throw::enter(player * _player)
 {
 	_player->setImage(IMAGEMANAGER->findImage("red_throw"));
+
+	_rc = RectMakeCenter(_player->getX(), _player->getY(), _player->getImage()->getFrameWidth(),
+		_player->getImage()->getFrameHeight());
+	_player->setRect(_rc);
+
+	_count = _index = _time = 0;
+
+	if (_player->isRight == true)
+	{
+		_player->getImage()->setFrameX(_index);
+		_player->getImage()->setFrameY(0);
+	}
+
+	if (_player->isRight == false)
+	{
+		_player->getImage()->setFrameX(_index);
+		_player->getImage()->setFrameY(1);
+	}
 }
