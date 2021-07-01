c736f523f45da420d9e64b0d004bc7e4abe65e26
박동철 <ehdcjf1123@naver.com>
2021-07-01 12:49:10 +0900

    red_catchAttackState클래스 추가

---
+++ ninja_baseball/red_catchAttackState.cpp
@@ -0,0 +1,15 @@
+#include "stdafx.h"
+#include "red_catchAttackState.h"
+
+playerstate * red_catchAttackState::handleInput(player * _player)
+{
+	return nullptr;
+}
+
+void red_catchAttackState::update(player * _player)
+{
+}
+
+void red_catchAttackState::enter(player * _player)
+{
+}
---
+++ ninja_baseball/red_catchAttackState.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "playerstate.h"
+
+class red_catchAttackState : public playerstate
+{
+private:
+	RECT _rc;
+	int _count;
+	int _index;
+	int _time;
+	
+public:
+	playerstate* handleInput(player* _player);
+	void update(player* _player);
+	void enter(player* _player);
+};
+
