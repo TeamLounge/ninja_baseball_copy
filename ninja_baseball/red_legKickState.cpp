8dc8968d87e3ec2955f68b6d352bd5699272dfca
DESKTOP-8LB6QCG\dong8 <ehdcjf1123@naver.com>
2021-07-03 20:31:03 +0900

    red_legKickState수정

--- ninja_baseball/red_legKickState.cpp
+++ ninja_baseball/red_legKickState.cpp
@@ -93,17 +93,17 @@ void red_legKickState::update(player * _player)
 	{
 		_player->setShadowX(_player->getX() - (_player->_shadow->getWidth() / 2) + 15 + IMAGEMANAGER->findImage("red_shadow")->getWidth() / 2);
 	}
+
+	_rc = RectMakeCenter(_player->getX(), _player->getY(), _player->getImage()->getFrameWidth(),
+		_player->getImage()->getFrameHeight());
+	_player->setRect(_rc);
 }
 
 void red_legKickState::enter(player * _player)
 {
 	_player->setImage(IMAGEMANAGER->findImage("red_legKick"));
 	_player->setImageName("red_legKick");
-		
-	_rc = RectMakeCenter(_player->getX(), _player->getY(), _player->getImage()->getFrameWidth(),
-	_player->getImage()->getFrameHeight());
-	_player->setRect(_rc);
-
+	
 	_count = _index = _time = 0;
 	
 	if (_player->isRight == true)
