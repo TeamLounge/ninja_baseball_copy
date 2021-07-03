5bfc92673c2cc9def7fedc8175ffb361126fcd92
YeaRin An <yearinan@gmail.com>
2021-07-03 18:34:07 +0900

    렌더매니저 알파블렌드 추가

--- ninja_baseball/renderManager.cpp
+++ ninja_baseball/renderManager.cpp
@@ -22,33 +22,65 @@ void renderManager::render(HDC hdc)
 	{
 		if (_arrObj[i].isNotHaveCurrentFrame)
 		{
-			IMAGEMANAGER->findImage(_arrObj[i].shadowImageName)->render(hdc,
-				*_arrObj[i].shadowX - (IMAGEMANAGER->findImage(_arrObj[i].shadowImageName)->getWidth() / 2),
-				*_arrObj[i].shadowY - (IMAGEMANAGER->findImage(_arrObj[i].shadowImageName)->getHeight() / 2));
-
-			if (_arrObj[i].isFrameImage)
+			if (_arrObj[i].isAlphaBlend)
 			{
-				IMAGEMANAGER->findImage(_arrObj[i].bodyImageName)->frameRender(hdc, *_arrObj[i].bodyX - (IMAGEMANAGER->findImage(_arrObj[i].bodyImageName)->getFrameWidth() / 2), 
-					*_arrObj[i].bodyY - (IMAGEMANAGER->findImage(_arrObj[i].bodyImageName)->getFrameHeight() / 2));
+				IMAGEMANAGER->findImage(_arrObj[i].shadowImageName)->render(hdc,
+					*_arrObj[i].shadowX - (IMAGEMANAGER->findImage(_arrObj[i].shadowImageName)->getWidth() / 2),
+					*_arrObj[i].shadowY - (IMAGEMANAGER->findImage(_arrObj[i].shadowImageName)->getHeight() / 2));
+				if (_arrObj[i].isFrameImage)
+				{
+					IMAGEMANAGER->findImage(_arrObj[i].bodyImageName)->alphaFrameRender(hdc, *_arrObj[i].bodyX - (IMAGEMANAGER->findImage(_arrObj[i].bodyImageName)->getFrameWidth() / 2),
+						*_arrObj[i].bodyY - (IMAGEMANAGER->findImage(_arrObj[i].bodyImageName)->getFrameHeight() / 2), *_arrObj[i].alpha);
+				}
+				else
+				{
+					IMAGEMANAGER->findImage(_arrObj[i].bodyImageName)->alphaRender(hdc, *_arrObj[i].bodyX - (IMAGEMANAGER->findImage(_arrObj[i].bodyImageName)->getWidth() / 2),
+						*_arrObj[i].bodyY - (IMAGEMANAGER->findImage(_arrObj[i].bodyImageName)->getHeight() / 2), *_arrObj[i].alpha);
+				}
 			}
 			else
-			{
-				IMAGEMANAGER->findImage(_arrObj[i].bodyImageName)->render(hdc, *_arrObj[i].bodyX - (IMAGEMANAGER->findImage(_arrObj[i].bodyImageName)->getWidth() / 2),
-					*_arrObj[i].bodyY - (IMAGEMANAGER->findImage(_arrObj[i].bodyImageName)->getHeight() / 2));
-			}
-		}
-		else
-		{
-			if (strcmp(_arrObj[i].shadowImageName, ""))
 			{
 				IMAGEMANAGER->findImage(_arrObj[i].shadowImageName)->render(hdc,
 					*_arrObj[i].shadowX - (IMAGEMANAGER->findImage(_arrObj[i].shadowImageName)->getWidth() / 2),
 					*_arrObj[i].shadowY - (IMAGEMANAGER->findImage(_arrObj[i].shadowImageName)->getHeight() / 2));
 
+				if (_arrObj[i].isFrameImage)
+				{
+					IMAGEMANAGER->findImage(_arrObj[i].bodyImageName)->frameRender(hdc, *_arrObj[i].bodyX - (IMAGEMANAGER->findImage(_arrObj[i].bodyImageName)->getFrameWidth() / 2),
+						*_arrObj[i].bodyY - (IMAGEMANAGER->findImage(_arrObj[i].bodyImageName)->getFrameHeight() / 2));
+				}
+				else
+				{
+					IMAGEMANAGER->findImage(_arrObj[i].bodyImageName)->render(hdc, *_arrObj[i].bodyX - (IMAGEMANAGER->findImage(_arrObj[i].bodyImageName)->getWidth() / 2),
+						*_arrObj[i].bodyY - (IMAGEMANAGER->findImage(_arrObj[i].bodyImageName)->getHeight() / 2));
+				}
 			}
-			IMAGEMANAGER->findImage(_arrObj[i].bodyImageName)->frameRender(hdc,
-				*_arrObj[i].bodyX,
-				*_arrObj[i].bodyY, *_arrObj[i].currentFrameX, *_arrObj[i].currentFrameY);
+		}
+		else
+		{
+			if (_arrObj[i].isAlphaBlend)
+			{
+				IMAGEMANAGER->findImage(_arrObj[i].shadowImageName)->render(hdc,
+					*_arrObj[i].shadowX - (IMAGEMANAGER->findImage(_arrObj[i].shadowImageName)->getWidth() / 2),
+					*_arrObj[i].shadowY - (IMAGEMANAGER->findImage(_arrObj[i].shadowImageName)->getHeight() / 2));
+				IMAGEMANAGER->findImage(_arrObj[i].bodyImageName)->alphaFrameRender(hdc, *_arrObj[i].bodyX - (IMAGEMANAGER->findImage(_arrObj[i].bodyImageName)->getFrameWidth() / 2),
+					*_arrObj[i].bodyY - (IMAGEMANAGER->findImage(_arrObj[i].bodyImageName)->getFrameHeight() / 2), *_arrObj[i].currentFrameX, *_arrObj[i].currentFrameY, *_arrObj[i].alpha);
+
+			}
+			else
+			{
+				if (strcmp(_arrObj[i].shadowImageName, ""))
+				{
+					IMAGEMANAGER->findImage(_arrObj[i].shadowImageName)->render(hdc,
+						*_arrObj[i].shadowX - (IMAGEMANAGER->findImage(_arrObj[i].shadowImageName)->getWidth() / 2),
+						*_arrObj[i].shadowY - (IMAGEMANAGER->findImage(_arrObj[i].shadowImageName)->getHeight() / 2));
+
+				}
+				IMAGEMANAGER->findImage(_arrObj[i].bodyImageName)->frameRender(hdc,
+					*_arrObj[i].bodyX,
+					*_arrObj[i].bodyY, *_arrObj[i].currentFrameX, *_arrObj[i].currentFrameY);
+			}
+			
 		}
 	}
 }
@@ -64,6 +96,34 @@ void renderManager::addObj(string strKey, const char* bodyImageName, const char*
 	object.shadowY = shadowY;
 	object.isNotHaveCurrentFrame = true;
 	object.isFrameImage = isFrameImage;
+	object.isAlphaBlend = false;
+
+	for (mapObjIter iter = _mObjList.begin(); iter != _mObjList.end(); iter++)
+	{
+		if (iter->first == strKey)
+		{
+			iter->second.push_back(object);
+			return;
+		}
+	}
+	arrObj array;
+	array.push_back(object);
+	_mObjList.insert(make_pair(strKey, array));
+}
+
+void renderManager::addObj(string strKey, const char * bodyImageName, const char * shadowImageName, float * bodyX, float * bodyY, float * shadowX, float * shadowY, bool isFrameImage, int * alphaBlend)
+{
+	OBJ object;
+	object.bodyImageName = bodyImageName;
+	object.shadowImageName = shadowImageName;
+	object.bodyX = bodyX;
+	object.bodyY = bodyY;
+	object.shadowX = shadowX;
+	object.shadowY = shadowY;
+	object.isNotHaveCurrentFrame = true;
+	object.isFrameImage = isFrameImage;
+	object.isAlphaBlend = true;
+	object.alpha = alphaBlend;
 
 	for (mapObjIter iter = _mObjList.begin(); iter != _mObjList.end(); iter++)
 	{
@@ -91,6 +151,7 @@ void renderManager::addObj(string strKey, const char* bodyImageName, const char*
 	object.currentFrameY = currentFrameY;
 	object.isNotHaveCurrentFrame = false;
 	object.isFrameImage = true;
+	object.isAlphaBlend = false;
 
 	for (mapObjIter iter = _mObjList.begin(); iter != _mObjList.end(); iter++)
 	{
@@ -104,6 +165,35 @@ void renderManager::addObj(string strKey, const char* bodyImageName, const char*
 	array.push_back(object);
 	_mObjList.insert(make_pair(strKey, array));
 
+}
+
+void renderManager::addObj(string strKey, const char * bodyImageName, const char * shadowImageName, float * bodyX, float * bodyY, float * shadowX, float * shadowY, int * currentFrameX, int * currentFrameY, int * alphaBlend)
+{
+	OBJ object;
+	object.bodyImageName = bodyImageName;
+	object.shadowImageName = shadowImageName;
+	object.bodyX = bodyX;
+	object.bodyY = bodyY;
+	object.shadowX = shadowX;
+	object.shadowY = shadowY;
+	object.currentFrameX = currentFrameX;
+	object.currentFrameY = currentFrameY;
+	object.isNotHaveCurrentFrame = false;
+	object.isFrameImage = true;
+	object.isAlphaBlend = true;
+	object.alpha = alphaBlend;
+
+	for (mapObjIter iter = _mObjList.begin(); iter != _mObjList.end(); iter++)
+	{
+		if (iter->first == strKey)
+		{
+			iter->second.push_back(object);
+			return;
+		}
+	}
+	arrObj array;
+	array.push_back(object);
+	_mObjList.insert(make_pair(strKey, array));
 }
 
 bool renderManager::deleteObj(string strKey, int i)
